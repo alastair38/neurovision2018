7bf5f4a7dee3a6b9bc479ec846be86b65490068c
alastair38 <alastair@alastaircox.com>
2018-03-13 13:00:47 +0000

    Version 1.37 - Old IE fixes (to test)

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!-->   <html class="no-js"  <?php language_attributes(); ?>> <!--<![endif]-->
 
-  <html class="no-js"  <?php language_attributes(); ?>>
 
 	<head>
 		<meta charset="utf-8">
@@ -25,6 +28,16 @@
 	    <?php } ?>
 
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+
+            <!--[if lt IE 9]>
+        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/styleie.css" />
+         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+        <script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
+        <script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+        <![endif]-->
 
   		<?php wp_head(); ?>
 
